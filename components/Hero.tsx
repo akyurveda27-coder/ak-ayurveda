6a7995be851c5d5af118475ae8535336195d0e50
AK Ayurveda <akyurveda27@gmail.com>
2026-08-14 07:44:29 +0000

    remove scroll indicator from hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -95,13 +95,7 @@ export default function Hero({ content }: HeroProps) {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-1 animate-bounce">
-        <span className="text-sage text-xs font-body">Scroll</span>
-        <svg className="w-4 h-4 text-sage" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-        </svg>
-      </div>
+      {/* Scroll indicator removed */}
     </section>
   )
 }
